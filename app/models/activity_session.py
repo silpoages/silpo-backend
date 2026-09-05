74e957027fb98bac2f6afae3a874e9ec560ce7b9
Ramiro Trejes <ramirotrejes@gmail.com>
2026-09-05 15:31:32 -0300

    fix: add default uuid4 missing in ActivitySession

--- app/models/activity_session.py
+++ app/models/activity_session.py
@@ -11,9 +11,7 @@ from app.db.base import Base
 class ActivitySession(Base):
     __tablename__ = "activity_session"
 
-    id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True
-    )
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
     user_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), ForeignKey("user.id"), nullable=False
