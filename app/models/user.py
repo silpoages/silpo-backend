444eaf5c5f1d9e842ccf080381eb6f2f27254a95
Ramiro Trejes <ramirotrejes@gmail.com>
2026-09-06 17:16:19 -0300

    fix: user table columns with wrong nullable value

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import date, datetime
 
-from sqlalchemy import Boolean, Date, DateTime, Enum, Text, func
+from sqlalchemy import Boolean, Date, DateTime, Enum, Text, func, text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -13,21 +13,23 @@ class User(Base):
     __tablename__ = "user"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    full_name: Mapped[str] = mapped_column(Text, nullable=False)
+    full_name: Mapped[str | None] = mapped_column(Text)
     gender: Mapped[Gender] = mapped_column(
         Enum(Gender, name="gender", values_callable=lambda enum: [e.value for e in enum]),
         nullable=False,
+        server_default=text(f"'{Gender.PREFER_NOT_TO_SAY.value}'"),
     )
-    birth_date: Mapped[date] = mapped_column(Date, nullable=False)
+    birth_date: Mapped[date | None] = mapped_column(Date)
     email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
-    password: Mapped[str | None] = mapped_column(Text, nullable=True)
-    phone_number: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(Text, nullable=False)
+    phone_number: Mapped[str | None] = mapped_column(Text, unique=True)
     role: Mapped[Role] = mapped_column(
         Enum(Role, name="role", values_callable=lambda enum: [e.value for e in enum]),
         nullable=False,
+        server_default=text(f"'{Role.USER.value}'"),
     )
-    profile_picture_url: Mapped[str | None] = mapped_column(Text, nullable=True)
-    plan_end_date: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
+    profile_picture_url: Mapped[str | None] = mapped_column(Text)
+    plan_end_date: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
     enabled: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default="true")
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
@@ -35,4 +37,4 @@ class User(Base):
     onboarding_completed: Mapped[bool] = mapped_column(
         Boolean, nullable=False, server_default="false"
     )
-    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
