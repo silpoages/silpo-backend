27ed5409233f407961e804f1319e032fa99073ff
Ramiro Trejes <ramirotrejes@gmail.com>
2026-09-18 20:39:45 -0300

    chore: use testcontainers.community.postgres to avoid deprecation warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ import pytest_asyncio
 from alembic.config import Config
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from testcontainers.postgres import PostgresContainer
+from testcontainers.community.postgres import PostgresContainer
 
 from alembic import command
 from app.api.deps import get_session
